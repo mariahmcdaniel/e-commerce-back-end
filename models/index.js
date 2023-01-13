acb4b01f390ccc45376569ef73ca12064cb654bb
Mariah <111791344+mariahmcdaniel@users.noreply.github.com>
2023-01-13 16:07:23 -0500

    models,associations set up

--- models/index.js
+++ models/index.js
@@ -5,6 +5,21 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category);
+
+Category.hasMany(Product);
+
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+  },
+});
+
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+  },
+});
 
 // Categories have many Products
 
