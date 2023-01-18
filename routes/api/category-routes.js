22f3ad7a9dd6c76355deb6113cefe215230e7f67
Mariah <111791344+mariahmcdaniel@users.noreply.github.com>
2023-01-18 18:27:30 -0500

    route edits

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -53,11 +53,12 @@ router.put('/:id', async (req, res) => {
   try {
     const categoryData = await Category.update(req.body, {
       where: {
-        id: req.params.id,
-      },
+        id: req.params.id
+      }
     });
-    if (!categoryData[0]) {
+    if (!categoryData) {
       res.status(404).json({ message: 'No category found with this id!' });
+      return;
     }
     res.status(200).json(categoryData);
   } catch (err) {
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -60,7 +60,7 @@ router.post('/', async (req, res) => {
 router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
   try {
-    const tagData = await Tag.update(
+    const tagData = await Tag.update(req.body,
       {
         tag_name: req.body.tag_name,
       },
